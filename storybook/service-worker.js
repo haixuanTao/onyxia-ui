4220bc53737b7aea6e73bb7d7cfbd4f9a44181c8
travis <>
2020-06-02 10:01:49 +0000

    Rebuild doc for revision 442f800b263333b2607ff1f2c73f416d98e5ab99: Select region (#140)

--- storybook/service-worker.js
+++ storybook/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.dd9e6c28187fe66bacd398d4e1291af8.js"
+  "precache-manifest.a9df1dab2379715ea6c2983b465310bd.js"
 );
 
 self.addEventListener('message', (event) => {
