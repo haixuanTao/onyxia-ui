66adb5aee7fb3ae3be897eaa0874ca829921c775
travis <>
2020-06-08 16:04:38 +0000

    Rebuild doc for revision 4d2a918b86969d0ebff9942983c604b2fc3b490e: Display post install instructions (#146)

---
+++ storybook/precache-manifest.e7f09d4a24b6b1008b576cf72c83d33c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "664db1c88618c1839b2e6c81b2aeacd0",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a7761e9606c353b1c087.bundle.js"
+  },
+  {
+    "url": "runtime~main.a7761e9606c353b1c087.bundle.js"
+  },
+  {
+    "revision": "352544d3eddca4724edf",
+    "url": "static/css/main.6054f80a.chunk.css"
+  },
+  {
+    "revision": "195a98eb59d3b70bb418",
+    "url": "static/css/vendors~main.7c35e809.chunk.css"
+  },
+  {
+    "url": "vendors~main.a7761e9606c353b1c087.bundle.js"
+  },
+  {
+    "url": "vendors~main.a7761e9606c353b1c087.bundle.js.LICENSE.txt"
+  }
+]);
--- storybook/service-worker.js
+++ storybook/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.66ffae91918bd4cdac792e24aa5fa9f4.js"
+  "precache-manifest.e7f09d4a24b6b1008b576cf72c83d33c.js"
 );
 
 self.addEventListener('message', (event) => {
